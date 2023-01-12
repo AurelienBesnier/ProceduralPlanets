55eaf7a50046b723dcda2ea3c1bd01dfd88e73bc
Aurelien Besnier <aurelienbesnier.pro@gmail.com>
2023-01-12 23:21:10 +0100

    Update help string

--- src/PlanetViewer.cpp
+++ src/PlanetViewer.cpp
@@ -411,25 +411,21 @@ QString PlanetViewer::helpString () const
 	QString text ("<h2>Procedural Planets</h2>");
 	text += "Use the mouse to move the camera around the object. ";
 	text +=
-			"You can respectively revolve around, zoom and translate with the three mouse buttons. ";
+			"You can respectively revolve around and translate with the two mouse buttons. ";
 	text +=
-			"Left and middle buttons pressed together rotate around the camera view direction axis<br><br>";
+			"Left button pressed together rotate around the camera view direction axis<br><br>";
 	text +=
-			"Pressing <b>Alt</b> and one of the function keys (<b>F1</b>..<b>F12</b>) defines a camera keyFrame. ";
+			"Press <b>F</b> to toggle the frame rate, <b>A</b> for the world axis, ";
 	text +=
-			"Simply press the function key again to restore it. Several keyFrames define a ";
+			"<b>Alt+Return</b> for full screen mode and <b>Control+S</b> to save a snapshot.";
 	text +=
-			"camera path. Paths are saved when you quit the application and restored at next start.<br><br>";
+			"See the <b>Keyboard</b> tab in this window for a complete shortcut list.";
 	text +=
-			"Press <b>F</b> to display the frame rate, <b>A</b> for the world axis, ";
-	text +=
-			"<b>Alt+Return</b> for full screen mode and <b>Control+S</b> to save a snapshot. ";
-	text +=
-			"See the <b>Keyboard</b> tab in this window for a complete shortcut list.<br><br>";
+			"Holding the <b>M</b> key with the viewer selected will move the plates continuously. Pressing <b>C</b> will center the planet in the viewer.";
+    text +=            
+            "Press the <b>W</b> key to change the display mode (between wireframe and solid).<br><br>";
 	text +=
 			"Double clicks automates single click actions: A left button double click aligns the closer axis with the camera (if close enough). ";
-	text +=
-			"A middle button double click fits the zoom of the camera and the right button re-centers the scene.<br><br>";
 	text +=
 			"A left button double click while holding right button pressed defines the camera <i>Revolve Around Point</i>. ";
 	text +=
